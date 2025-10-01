bbee930d52f8fc5e07d7c76d8ece31322abc3f26
Juliano Galhardo de Oliveira <julianogalhardo@192.168.1.3>
2025-10-01 14:14:26 -0300

    separation of controllers done

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,19 +1,9 @@
-// A palavra 'export' torna esta variável 'tickets' acessível para outros arquivos do seu projeto.
-// 'const tickets = [' inicia a criação de uma lista (array) de rotas.
+import { create } from "../controllers/tickets/create.js"
+
 export const tickets = [
-    // Abaixo está o primeiro objeto da lista, que representa uma rota específica.
     {
-        // Define o método HTTP para esta rota. 'POST' é geralmente usado para criar algo novo no servidor.
         method: "POST",
-
-        // Define o caminho na URL que ativa esta rota (ex: www.seusite.com/tickets).
         path: "/tickets",
-
-        // 'controller' é a função que será executada quando a rota for acessada com o método e caminho corretos.
-        controller: (request, response) => {
-
-            // 'response.end()' envia uma resposta simples de texto para o navegador/cliente e encerra a conexão.
-            response.end("Criado com sucesso!")
-        },
+        controller: create,
     },
 ]
--- src/server.js
+++ src/server.js
@@ -1,19 +1,10 @@
-// Importa o módulo 'http' nativo do Node.js para criar um servidor web.
 import http from "node:http"
 
-// Importa uma função 'middleware' que vai processar dados em formato JSON.
 import { jsonHandler } from "./middlewares/jsonHandler.js"
 import { routeHandler } from "./middlewares/routeHandler.js"
 
-// Define a função principal que será executada para cada requisição recebida.
 async function listener(request, response) {
-
-    // Espera o middleware processar o corpo da requisição (ex: converter de JSON).
     await jsonHandler(request, response)
-
-    // Exibe no terminal o corpo da requisição já processado.
     routeHandler(request, response)
 }
-
-// Cria o servidor usando a função 'listener' e o faz "escutar" por conexões na porta 3333.
 http.createServer(listener).listen(3333)
