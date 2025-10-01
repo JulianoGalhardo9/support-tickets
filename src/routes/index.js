22779690ba2cc6299c6049735e6823f8cd2a63b5
Juliano Galhardo de Oliveira <julianogalhardo@192.168.1.3>
2025-10-01 17:04:19 -0300

    dynamic parameters

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,7 @@
-// Importa a lista de rotas que você definiu no arquivo 'tickets.js'.
-// Agora, a variável 'tickets' existe neste arquivo e contém aquela lista.
+import { parseRoutePath } from "../utils/parseRoutePath.js";
 import { tickets } from "./tickets.js";
 
-// Cria e exporta uma nova lista chamada 'routes'.
-// O '...' (spread syntax) "espalha" ou "despeja" todos os itens
-// da lista 'tickets' dentro desta nova lista 'routes'.
-// Em resumo, está fazendo uma cópia da lista de rotas importada.
-export const routes = [...tickets]
+export const routes = [...tickets].map((route) => ({
+    ...route,
+    path: parseRoutePath(route.path)
+}))
